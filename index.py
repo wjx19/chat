fd3d355cb8f110884e6cd80d6244372edde53b50
wjx19 <40166304+wjx19@users.noreply.github.com>
2023-05-05 20:15:17 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,38 @@
+#import sys
+#sys.path.append("/home/azureuser/lib")
+
+from flask import Flask,request
+import requests
+import json
+import random
+import openai
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '在此输入apikey 样式:sk-s5S5BoV...'
+
+@app.route('/message',methods = ['POST'])
+def mess():  # put application's code here
+    message = request.json.get('msg')
+    openai.api_key = "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" #修改这里为自己申请的api_key
+    messages = [
+        {"role": "system", "content": "You are a coding tutor bot to help user write and optimize python code."},
+    ]
+    messages.append({"role": "user", "content": message})
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[{"role": "user", "content": message}]
+    )
+
+    print(completion)
+    #messages.append({"role": "assistant", "content": response["choices"][0]["message"].content})
+    res = {
+            "resmsg":completion,
+            "code":200
+            }
+    return res
+if __name__ == '__main__':
+    app.run(threaded = False,processes=5,host="0.0.0.0",port="5000", ssl_context=('wjxing_me.crt','key.txt'))
